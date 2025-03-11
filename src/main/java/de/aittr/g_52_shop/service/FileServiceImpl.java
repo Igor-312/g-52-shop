6513f907373d07dcf264ef0c6e065d1ec5879138
IgorSv <igorexa08@gmail.com>
2025-03-11 12:32:07 +0100

    commit -m "add shop"

--- src/main/java/de/aittr/g_52_shop/service/FileServiceImpl.java
+++ src/main/java/de/aittr/g_52_shop/service/FileServiceImpl.java
@@ -6,6 +6,8 @@ import de.aittr.g_52_shop.service.interfaces.ProductService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.UUID;
+
 @Service
 public class FileServiceImpl implements FileService {
 
@@ -20,6 +22,15 @@ public class FileServiceImpl implements FileService {
     @Override
     public String upload(MultipartFile file, String productTitle) {
         return "";
+    }
+
+    private String generateUniqueFileName(MultipartFile file) {
+        String sourceFilename = file.getOriginalFilename();
+        int dotIndex = sourceFilename.lastIndexOf(".");
+        String fileName = sourceFilename.substring(0, dotIndex);
+        String extension = sourceFilename.substring(dotIndex);
+
+        return String.format("%s-%s%s", fileName, UUID.randomUUID(), extension);
     }
 
 
